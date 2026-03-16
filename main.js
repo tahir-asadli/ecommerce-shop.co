9cc16069069474d4d7259e5f4a0bcced185fa3a4
Tahir Asadli <tahir-asadov@outlook.com>
2026-03-16 16:08:39 +0400

    Update header navigation menu

--- main.js
+++ main.js
@@ -5,4 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
   closeButton?.addEventListener('click', function () {
     announcement && (announcement.style.display = 'none');
   });
+
+  const headerLinks = document.querySelectorAll('.header-link');
+
+  headerLinks.forEach((link) => {
+    link.addEventListener('mouseenter', function () {
+      const content = this.querySelector('.header-link-content');
+      if (content) {
+        // Update inner scroll height
+        const contentHeight = content.scrollHeight;
+        content.style.height = contentHeight + 'px';
+        content.classList.add('header-content-visible');
+      }
+    });
+
+    link.addEventListener('mouseleave', function () {
+      const content = this.querySelector('.header-link-content');
+      if (content) {
+        content.classList.remove('header-content-visible');
+        content.style.height = '0';
+      }
+    });
+  });
 });
